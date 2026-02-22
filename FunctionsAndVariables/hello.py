effb98dbaf6bd903b32aed84d4bc185da620f6be
LivStokes <Oliviastokess02@gmail.com>
2026-02-22 19:19:28 +0000

    Week 0 Lecture

---
+++ FunctionsAndVariables/hello.py
@@ -0,0 +1,14 @@
+input("What's your name? ")
+print("hello, Olivia.")
+
+# Another alternative
+name = input("Whats your name? ")
+print("hello, name")
+# Or
+print("hello, " + name)
+# Or
+print("hello,", name)
+# Or
+print("hello, ")
+print(name)
+# Or
